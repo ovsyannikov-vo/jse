183a9aec68948d9aa9e642e2dda6a488c149d0df
Vladislav Ovsyannikov <vldslv.ovsyannikov@gmail.com>
2020-07-28 14:43:57 +0300

    Добавлен мавен

--- src/ru/ovsyannikov/tm/constant/TerminalConst.java
+++
@@ -1,8 +0,0 @@
-package ru.ovsyannikov.tm.constant;
-
-public class TerminalConst {
-    public static final String HELP = "help";
-    public static final String ABOUT = "about";
-    public static final String VERSION = "version";
-
-}
