334ad1ad2a22c876cc42f58fed619e3ad5a8f87e
Vladislav Ovsyannikov <vldslv.ovsyannikov@gmail.com>
2020-08-04 14:29:00 +0300

    Реализованы 1. Главная петля приложения 2. Обработка ввода с клавиатуры (использован Scanner) 3. Проверка на пустые строки 4. Терминальная команда  "EXIT"

--- src/main/java/ru/ovsyannikov/tm/App.java
+++ src/main/java/ru/ovsyannikov/tm/App.java
@@ -1,54 +1,77 @@
 package ru.ovsyannikov.tm;
 
+import java.util.Scanner;
+
 import static ru.ovsyannikov.tm.constant.TerminalConst.*;
 
 public class App {
-    /*  10.07.2020
+    /*
         Task-Manager
-        version: 1.0.0
+        version: 1.0.1
         developer: Ovsyannikov Vladislav
-     */
+    */
 
     public static void main(final String[] args) {
         run(args);
         displayWelcome();
+        final Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while (!EXIT.equals(command)) {
+            command = scanner.nextLine();
+            run(command);
+        }
     }
 
     private static void run(final String[] args) {
         if (args.length == 0) return;
         if (args.length <1) return;
         final String param=args[0];
+        final int result = run(param);
+        System.exit(result);
+    }
+
+    private static int run(final String param) {
+        if (param == null) return -1;
         switch (param){
-            case VERSION: displayVersion();
-            case ABOUT:  displayAbout();
-            case HELP: displayHelp();
-            default: displayError();
+            case VERSION: return displayVersion();
+            case ABOUT:  return displayAbout();
+            case HELP: return displayHelp();
+            case EXIT: return displayExit();
+            default: return displayError();
         }
     }
-    private static void displayError(){
-        System.out.println("Error! Unknown program argument...");
-        System.exit(-1);
+
+    private static int displayExit() {
+        System.out.println("Terminate program...");
+        return 0;
     }
-    private static void displayWelcome(){
+
+    private static int displayError() {
+        System.out.println("Error! Unknown program argument...");
+        return -1;
+    }
+
+    private static void displayWelcome() {
         System.out.println("** Welcome to Task-Manager **");
     }
 
-    private static void displayAbout(){
+    private static int displayAbout() {
         System.out.println("Ovsyannikov Vladislav");
         System.out.println("vldslv.ovsyannikov@gmail.com");
-        System.exit(0);
+        return 0;
     }
 
-    private static void displayVersion(){
-        System.out.println("1.0.0");
-        System.exit(0);
+    private static int displayVersion() {
+        System.out.println("1.0.1");
+        return 0;
     }
 
-    private static void displayHelp(){
+    private static int displayHelp() {
         System.out.println("vesrsion - Display application version");
         System.out.println("about - Display developer info");
         System.out.println("help - Display list of commands");
-        System.exit(0);
+        System.out.println("exit - Terminate console application");
+        return 0;
     }
 
 }
